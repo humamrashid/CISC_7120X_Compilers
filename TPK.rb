282a6d5f213299fe502a22956429add494f2ac58
Humam Rashid <humamrashid@gmail.com>
2019-09-10 15:41:57 -0400

    Incremental commit.

--- TPK.rb
+++ TPK.rb
@@ -11,4 +11,7 @@ S.reverse.each do |e|
   puts (val > 500) ? "f(#{e}): caused overflow!" : "f(#{e}) = #{val}"
 end
 
+puts("Enter 11 numbers: ")
+(S = 11.times.map { gets.to_f }).reverse.each { |e| puts ((val = ->x { Math.sqrt(x.abs) + (5 * x) }.call(e)) > 500) ? "f(#{e}): overflow!" : "f(#{e}) = #{val}" }
+
 # EOF.
