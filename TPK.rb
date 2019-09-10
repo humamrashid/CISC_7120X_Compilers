86765821bdad64571ac7d466b1c3015ed6d7ec1c
Humam Rashid <humamrashid@gmail.com>
2019-09-10 15:50:46 -0400

    Incremental commit.

--- TPK.rb
+++ TPK.rb
@@ -2,16 +2,6 @@
 
 # An implementation of Trabb Pardo-Knuth algorithm in Ruby.
 
-S = []
-puts("Enter 11 numbers: ")
-11.times { S.push(gets.to_f) }
-puts("Result: ")
-S.reverse.each do |e|
-  val = ->(x) { Math.sqrt(x.abs) + (5 * x) }.call(e)
-  puts (val > 500) ? "f(#{e}): caused overflow!" : "f(#{e}) = #{val}"
-end
-
-puts("Enter 11 numbers: ")
-(S = 11.times.map { gets.to_f }).reverse.each { |e| puts ((val = ->x { Math.sqrt(x.abs) + (5 * x) }.call(e)) > 500) ? "f(#{e}): overflow!" : "f(#{e}) = #{val}" }
+puts("Enter 11 numbers: ") or (S = 11.times.map { gets.to_f }).reverse.each { |e| puts ((val = ->x { Math.sqrt(x.abs) + (5 * x) }.call(e)) > 500) ? "f(#{e}): overflow!" : "f(#{e}) = #{val}" }
 
 # EOF.
