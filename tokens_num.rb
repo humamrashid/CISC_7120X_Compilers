4f0555013c756f52036ca9761a11b443f1d49514
Humam Rashid <humamrashid@gmail.com>
2019-10-16 18:37:14 -0400

    Added tokens_num.rb string token counter.

---
+++ tokens_num.rb
@@ -0,0 +1,13 @@
+#!/usr/bin/ruby
+#
+# Humam Rashid
+# Fall 2019, CISC 7120X
+# Answer to question 3, mid-term exam.
+
+def tokens_num(string)
+  string.split.each { |s|
+  }
+end
+
+print "Enter a strings: "
+puts tokens_num(gets.chomp)
