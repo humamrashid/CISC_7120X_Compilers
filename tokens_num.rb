89e7b840f4ce2c2552960629f46d4093e26de5bd
Humam Rashid <humamrashid@gmail.com>
2019-10-17 22:23:46 -0400

    Incremental commit

--- tokens_num.rb
+++ tokens_num.rb
@@ -2,12 +2,21 @@
 #
 # Humam Rashid
 # Fall 2019, CISC 7120X
-# Answer to question 3, mid-term exam.
+# Answer to question 3, mid-term exam. This implementation
+# uses regular expressions to determine token count.
 
 def tokens_num(string)
-  string.split.each { |s|
-  }
+  literal = /\A0\Z|\A[1-9]\d*\Z/
+  identifier = /\A([a-z]|[A-Z]|_)\w*\Z/
+  either = /(\A0\Z|\A[1-9]\d*\Z)|(\A([a-z]|[A-Z]|_)\w*\Z)/
+  [
+    string.split.grep(identifier).length,
+    string.split.grep(literal).length,
+    string.split.grep_v(either).length
+  ]
 end
 
-print "Enter a strings: "
-puts tokens_num(gets.chomp)
+print "Enter a string: "
+puts tokens_num(gets.chomp).to_s
+
+# EOF.
