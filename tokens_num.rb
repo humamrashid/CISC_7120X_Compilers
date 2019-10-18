e5555988300e8b9545ecf924f11cccd874f4bb8e
Humam Rashid <humamrashid@gmail.com>
2019-10-17 22:31:21 -0400

    Incremental commit

--- tokens_num.rb
+++ tokens_num.rb
@@ -6,13 +6,10 @@
 # uses regular expressions to determine token count.
 
 def tokens_num(string)
-  literal = /\A0\Z|\A[1-9]\d*\Z/
-  identifier = /\A([a-z]|[A-Z]|_)\w*\Z/
-  either = /(\A0\Z|\A[1-9]\d*\Z)|(\A([a-z]|[A-Z]|_)\w*\Z)/
   [
-    string.split.grep(identifier).length,
-    string.split.grep(literal).length,
-    string.split.grep_v(either).length
+    string.split.grep(/\A([a-z]|[A-Z]|_)\w*\Z/).length,
+    string.split.grep(/\A0\Z|\A[1-9]\d*\Z/).length,
+    string.split.grep_v(/(\A0\Z|\A[1-9]\d*\Z)|(\A([a-z]|[A-Z]|_)\w*\Z)/).length
   ]
 end
 
