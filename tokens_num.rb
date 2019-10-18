62a5632539ce54a1d408ae2acb17e46f26ca4842
Humam Rashid <humamrashid@gmail.com>
2019-10-17 23:24:21 -0400

    Added second implementation of tokens_num.

--- tokens_num.rb
+++ tokens_num.rb
@@ -13,7 +13,24 @@ def tokens_num(string)
   ]
 end
 
+def tokens_num2(string)
+  ids = lits = others = 0
+  string.split.each do |s|
+    case s
+    when /\A([a-z]|[A-Z]|_)\w*\Z/
+      ids += 1
+    when /\A0\Z|\A[1-9]\d*\Z/
+      lits += 1
+    else
+      others += 1
+    end
+  end
+  [ids, lits, others]
+end
+
 print "Enter a string: "
 puts tokens_num(gets.chomp).to_s
+print "Enter a string: "
+puts tokens_num2(gets.chomp).to_s
 
 # EOF.
