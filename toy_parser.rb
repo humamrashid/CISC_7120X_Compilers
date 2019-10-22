993cc6b8955c2ffef62faaed5b6cc1c4d7c6b54d
Humam Rashid <humamrashid@gmail.com>
2019-10-22 17:53:30 -0400

    Added toy grammar to related files.

--- toy_parser.rb
+++ toy_parser.rb
@@ -1,4 +1,26 @@
 #!/usr/bin/ruby
+# Parser for toy language.
+# Humam Rashid
+# CISC 7120X, Fall 2019.
+#
+# This file includes the Parser class.
+#
+# Grammar for toy language with left-recursion removed.
+#
+# Let e = epsilon, then:
+#
+# Program       -> Assignment*
+# Assignment    -> Identifier = Exp;
+# Exp           -> Term ExpPrime
+# ExpPrime      -> e | + Term ExpPrime | - Term ExpPrime
+# Term          -> Fact TermPrime
+# TermPrime     -> e | * Fact TermPrime
+# Fact          -> ( Exp ) | - Fact | + Fact | Literal | Identifier
+# Identifier    -> Letter [ Letter | Digit ]*
+# Letter        -> a | ... | z | A | ... | Z | _
+# Literal       -> 0 | NonZeroDigit Digit*
+# NonZeroDigit  -> 1 | ... | 9
+# Digit         -> 0 | 1 | ... | 9
 
 require_relative 'toy_lexer'
 
--- toylang_lex.c
+++ toylang_lex.c
@@ -7,6 +7,23 @@
  * 1. lexer design in chapter 1 of "Compielr Design in C" by Holub (1990).
  *
  * This implementation uses a lookahead.
+ * 
+ * Grammar for toy language with left-recursion removed.
+ *
+ * Let e = epsilon, then:
+ *
+ * Program       -> Assignment*
+ * Assignment    -> Identifier = Exp;
+ * Exp           -> Term ExpPrime
+ * ExpPrime      -> e | + Term ExpPrime | - Term ExpPrime
+ * Term          -> Fact TermPrime
+ * TermPrime     -> e | * Fact TermPrime
+ * Fact          -> ( Exp ) | - Fact | + Fact | Literal | Identifier
+ * Identifier    -> Letter [ Letter | Digit ]*
+ * Letter        -> a | ... | z | A | ... | Z | _
+ * Literal       -> 0 | NonZeroDigit Digit*
+ * NonZeroDigit  -> 1 | ... | 9
+ * Digit         -> 0 | 1 | ... | 9
  */
 #include <stdio.h>
 #include <ctype.h>
