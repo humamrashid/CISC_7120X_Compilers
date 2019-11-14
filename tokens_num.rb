cf5722731e1a17b05cc6947cf9ed72db5d152183
Humam Rashid <humamrashid@gmail.com>
2019-11-14 04:49:46 -0500

    Added picat hw.

--- tokens_num.rb
+++ tokens_num.rb
@@ -14,15 +14,6 @@ def tokens_num(string)
   ]
 end
 
-def tokens_num1(string)
-  s = string.split
-  [
-    s.grep(/\A([a-z]|[A-Z]|_)\w*\Z/).length,
-    s.grep(/\A0\Z|\A[1-9]\d*\Z/).length,
-    s.grep_v(/(\A0\Z|\A[1-9]\d*\Z)|(\A([a-z]|[A-Z]|_)\w*\Z)/).length
-  ]
-end
-
 print "Enter a string: "
 puts tokens_num(gets.chomp).to_s
 
