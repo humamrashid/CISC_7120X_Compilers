2d4a6b50a3f82c94d77fea1f8c67b1989622aff9
Humam Rashid <humamrashid@gmail.com>
2019-11-28 22:36:42 -0500

    Inc. commit.

--- CISC7120X_HW_Haskell.hs
+++ CISC7120X_HW_Haskell.hs
@@ -3,11 +3,9 @@
 -- Haskell Homework.
 
 -- 1. Get volume of a sphere given radius r.
-sphere :: Float -> Float
 sphere r = (4/3) * pi * (r**3)
 
--- 1. Get real roots of quadratic equation of the form ax^2+bx+c=0.
-quadratic_eq :: (Float, Float, Float) -> (Float, Float)
+-- 2. Get real roots of quadratic equation of the form ax^2+bx+c=0.
 quadratic_eq (a, b, c) =
     if d < 0 then error "discriminant < 0" else (r1, r2)
         where
@@ -15,5 +13,11 @@ quadratic_eq (a, b, c) =
             r2 = e - (sqrt d) / (2*a)
             d = (b**2) - (4*a*c)
             e = -b / (2*a)
+
+-- 3. Get number of zeroes in a list of numbers.
+num_of_zeroes [] = 0
+num_of_zeroes (h:t)
+    | h == 0 = 1 + (num_of_zeroes t)
+    | otherwise = num_of_zeroes t
 
 -- EOF.
