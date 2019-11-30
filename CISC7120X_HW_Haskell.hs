35db9187a999c4da335863e9b9015d7dfa9f7dc1
Humam Rashid <humamrashid@gmail.com>
2019-11-29 19:50:20 -0500

    Inc. commit.

--- CISC7120X_HW_Haskell.hs
+++ CISC7120X_HW_Haskell.hs
@@ -2,6 +2,8 @@
 -- CISC 7120X, Fall 2019.
 -- Haskell Homework.
 
+import Data.List
+
 -- 1. Get volume of a sphere given radius r.
 sphere r = (4/3) * pi * (r**3)
 
@@ -25,6 +27,14 @@ pascal = iterate (\row -> zipWith (+) ([0] ++ row) (row ++ [0])) [1]
 pascal_printer = mapM_ (putStrLn . unwords . map show)
 draw_pascal n = pascal_printer $ take n pascal
 
+-- 5. Find unique positive integer N whose square is 1_2_3_4_5_6_7_8_9_0 and
+-- each '_' is a single digit.
+judge n = s!!0=='1'  && s!!2=='2'  && s!!4=='3'  &&
+          s!!6=='4'  && s!!8=='5'  && s!!10=='6' &&
+          s!!12=='7' && s!!14=='8' && s!!16=='9' &&
+          s!!18=='0'
+    where s = show n
+euler206 = find (\n->judge$n^2) [1010101010,1010101020..1389026623]
 -- Helper functions:
 qsort [] = []
 qsort (x:xs) = qsort [y | y <- xs, y <= x]  ++ [x] ++ qsort [y | y <- xs, y > x]
