845921ed59fc858975d4f4e605aa848df049c264
Humam Rashid <humamrashid@gmail.com>
2019-12-09 23:17:53 -0500

    Inc. commit.

--- CISC7120X_HW_Python.py
+++ CISC7120X_HW_Python.py
@@ -4,6 +4,7 @@
 # CISC 7120X, Fall 2019.
 
 import math
+from datetime import date
 
 # 1. Get volume of a sphere given radius r.
 def sphere(r):
@@ -29,7 +30,6 @@ def next_row(row):
         row1.append(row[i]+row[i+1])
     row1.append(1)
     return row1
-
 def draw_pascal(n):
     row = [1]
     rows = [row]
@@ -38,9 +38,40 @@ def draw_pascal(n):
         rows.append(row)
     return rows
 
-print(sphere(5))
-print(quadratic_equation(2,5,2))
-print(number_of_zeroes([0,1,2,4,0,4,0,25,0]))
-print(draw_pascal(5))
+# 5. Find unique positive integer N whose square is 1_2_3_4_5_6_7_8_9_0 and each
+# '_' is a single digit.
+def is_concealed(n):
+    digits = str(n)
+    template = "1_2_3_4_5_6_7_8_9_0"
+    if len(digits) != len(template):
+        return False
+    return all(digits[i]==template[i] for i in range(0, len(digits), 2))
+def euler206():
+    print(next((n for n in range(1000000000, 1390000000, 10)
+        if (n % 100 == 30 or n % 100 == 70) and is_concealed(n*n))))
+
+# 6. Get number of days between two dates.
+def days(date1, date2):
+    return (date2-date1).days
+
+# 7. Remove consecutive duplicate elements from a given list.
+def remove_consecutive_dups(lst):
+    l = []
+    prev = None
+    for i in lst:
+        if i != prev:
+            l.append(i)
+        prev = i
+    return l
+
+# 8. Remove all duplicate elements from a given list.
+def remove_dups(lst):
+    s = {}
+    l = []
+    for e in lst:
+        if not (e in s):
+            s[e] = True
+            l.append(e)
+    return l
 
 # EOF.
