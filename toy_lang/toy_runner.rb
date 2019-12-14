e44165f89f52751c38b876654c597988666fe681
Humam Rashid <humamrashid@gmail.com>
2019-12-13 21:12:56 -0500

    Inc. commit.

--- toy_lang/toy_runner.rb
+++ toy_lang/toy_runner.rb
@@ -3,7 +3,7 @@
 # Toy Language Runner (running code for parser).
 #
 # Initial design partially based on L. Wrobel's 'Math
-# Parser' and Holub's 'Compiler Design in C' chapter 1.
+# Parser' and A. Holub's 'Compiler Design in C' chapter 1.
 #
 # Humam Rashid
 # CISC 7120X, Fall 2019.
