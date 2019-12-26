c9d2fff4be3e37cfcc888d4c01f3d835f8952f88
Humam Rashid <humamrashid@gmail.com>
2019-12-26 05:07:00 -0500

    Inc. commit.

--- tokens_num.rb
+++ tokens_num.rb
@@ -3,10 +3,11 @@
 # Counts number of identifiers, integer literals and other things in a given expression.
 
 def tokens_num(string)
+  s = string.split
   [
-    string.split.grep(/\A([a-z]|[A-Z]|_)\w*\Z/).length,
-    string.split.grep(/\A0\Z|\A[1-9]\d*\Z/).length,
-    string.split.grep_v(/(\A0\Z|\A[1-9]\d*\Z)|(\A([a-z]|[A-Z]|_)\w*\Z)/).length
+    s.grep(/\A([a-z]|[A-Z]|_)\w*\Z/).length,
+    s.grep(/\A0\Z|\A[1-9]\d*\Z/).length,
+    s.grep_v(/(\A0\Z|\A[1-9]\d*\Z)|(\A([a-z]|[A-Z]|_)\w*\Z)/).length
   ]
 end
 print "Enter a string: "
